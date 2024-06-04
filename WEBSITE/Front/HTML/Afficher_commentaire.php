1e34bcf5678d352d22436d1a51412ed29e82b30b
Gauthier Schmitt <gauthier.schmitt1@gmail.com>
2024-06-04 21:51:17 +0200

    push

--- WEBSITE/Front/HTML/Afficher_commentaire.php
+++ WEBSITE/Front/HTML/Afficher_commentaire.php
@@ -8,6 +8,7 @@
 <body>
 <?php
     include_once 'conf.php';
+    $bdd = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 
     // Déterminer la page actuelle
     $page = isset($_GET['page']) ? $_GET['page'] : 1;
--- WEBSITE/Front/HTML/conf.php
+++ WEBSITE/Front/HTML/conf.php
@@ -3,4 +3,3 @@ $host = getenv("MYSQL_HOST");
 $user = getenv("MYSQL_USER");
 $pass = getenv("MYSQL_PASSWORD");
 $db = getenv("MYSQL_DATABASE");
-$bdd = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
