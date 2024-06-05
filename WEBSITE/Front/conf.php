8df8e26a5024b6096e395cd4ff2666c8ca710ac4
Gauthier Schmitt <gauthier.schmitt1@gmail.com>
2024-06-06 01:50:46 +0200

    jj

--- WEBSITE/Front/conf.php
+++ WEBSITE/Front/conf.php
@@ -1,19 +1,17 @@
 <?php
 // Paramètres de connexion
-$host = 'herogu.garageisep.com'; // Adresse du serveur
-$db = 'dIr64sSdId_app_g9e'; // Nom de la base de données
-$user = 't3S8KneeKd_app_g9e'; // Nom d'utilisateur de la base de données
-$pass = 'M7fboKJOxkHzSLnr'; // Mot de passe de l'utilisateur de la base de données
+$host = 'herogu.garageisep.com';
+$db = 'dIr64sSdId_app_g9e';
+$user = 't3S8KneeKd_app_g9e';
+$pass = 'M7fboKJOxkHzSLnr';
 
-
-$dsn = "mysql:host=$host;dbname=$db"
+$dsn = "mysql:host=$host;dbname=$db";
 
 try {
-    // Création de l'instance PDO
     $bdd = new PDO($dsn, $user, $pass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connexion réussie à la base de données !";
 } catch (\PDOException $e) {
-    // Gestion des erreurs de connexion
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    echo "Erreur de connexion : " . $e->getMessage();
 }
 ?>
